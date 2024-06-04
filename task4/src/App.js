f508b9ea9b086dba0d94922b064a15e8fa52e808
Sivaraju35 <sivaraju7032@gmail.com>
2024-06-04 16:17:10 +0530

    Add files via upload

--- task4/src/App.js
+++ task4/src/App.js
@@ -1,56 +1,13 @@
-// App.js
-import React, { useState } from 'react';
-import './App.css'; // Import your CSS file for styling
+// src/App.js
+import React from 'react';
+import TodoList from './TodoList';
 
-function TodoList() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-
-  const handleInputChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const handleAddTask = () => {
-    if (newTask.trim() !== '') {
-      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
-      setNewTask('');
-    }
-  };
-
-  const handleCompleteTask = (taskId) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
-
+const App = () => {
   return (
-    <div className="todo-list">
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        placeholder="Add new task"
-        value={newTask}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleAddTask}>Add Task</button>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id} className={task.completed ? 'completed' : ''}>
-            <span onClick={() => handleCompleteTask(task.id)}>
-              {task.text}
-            </span>
-            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <TodoList />
     </div>
   );
-}
+};
 
-export default TodoList;
+export default App;
